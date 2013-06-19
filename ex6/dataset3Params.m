c4e96a64edeb3b10ee861616fc2f79adc36de546
Devin Kelly <dwwkelly@gmail.com>
2013-06-18 23:16:20 -0400

    finished first half of ex6

--- ex6/dataset3Params.m
+++ ex6/dataset3Params.m
@@ -8,26 +8,36 @@ function [C, sigma] = dataset3Params(X, y, Xval, yval)
 %
 
 % You need to return the following variables correctly.
-C = 1;
-sigma = 0.3;
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: Fill in this function to return the optimal C and sigma
 %               learning parameters found using the cross validation set.
 %               You can use svmPredict to predict the labels on the cross
-%               validation set. For example, 
+%               validation set. For example,
 %                   predictions = svmPredict(model, Xval);
 %               will return the predictions on the cross validation set.
 %
-%  Note: You can compute the prediction error using 
+%  Note: You can compute the prediction error using
 %        mean(double(predictions ~= yval))
 %
 
+Cvals = [0.01 0.03 0.1 0.3 1 3 10 30];
+sigmaVals = [0.01 0.03 0.1 0.3 1 3 10 30];
+lastErr = 9999999;
 
-
-
-
-
+for ind_c = 1:length(Cvals)
+   for ind_s = 1:length(sigmaVals)
+      sim =  @(x1, x2) gaussianKernel(x1, x2, sigmaVals(ind_s));
+      model = svmTrain(X, y, Cvals(ind_c), sim, 0.1, 5);
+      predictions = svmPredict(model, Xval);
+      err = mean(double(predictions ~= yval));
+      if err < lastErr
+         lastErr = err;
+         C = Cvals(ind_c);
+         sigma = sigmaVals(ind_s);
+      end
+   end
+end
 
 % =========================================================================
 
