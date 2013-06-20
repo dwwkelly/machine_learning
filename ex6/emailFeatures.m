53b5a16819b2e70ad205a950449877f906f09130
Devin Kelly <dwwkelly@gmail.com>
2013-06-19 23:17:33 -0400

    finished up exercise 6

--- ex6/emailFeatures.m
+++ ex6/emailFeatures.m
@@ -12,20 +12,20 @@ x = zeros(n, 1);
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: Fill in this function to return a feature vector for the
-%               given email (word_indices). To help make it easier to 
+%               given email (word_indices). To help make it easier to
 %               process the emails, we have have already pre-processed each
 %               email and converted each word in the email into an index in
 %               a fixed dictionary (of 1899 words). The variable
 %               word_indices contains the list of indices of the words
 %               which occur in one email.
-% 
+%
 %               Concretely, if an email has the text:
 %
 %                  The quick brown fox jumped over the lazy dog.
 %
-%               Then, the word_indices vector for this text might look 
+%               Then, the word_indices vector for this text might look
 %               like:
-%               
+%
 %                   60  100   33   44   10     53  60  58   5
 %
 %               where, we have mapped each word onto a number, for example:
@@ -48,14 +48,8 @@ x = zeros(n, 1);
 %
 %
 
-
-
-
-
-
-
+x(word_indices) = 1;
 
 % =========================================================================
-    
 
 end
